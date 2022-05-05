e3d5850939f8d0a0237a71fb2c58e0d88b51f3a9
Ahmed-Achtatar <ahmed.achtatar@usmba.ac.ma>
2022-05-05 15:10:14 +0000

    initialisation

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUSComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutUSComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    TechnologiesComponent,
+    ContactUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
