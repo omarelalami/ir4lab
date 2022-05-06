a8d0a20ccc61a5f86144bfee6159d3c228bd8361
omarelalami <omaralami230@gmail.com>
2022-05-06 15:27:37 +0000

    Services Technologie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'landing',
+component: TechnologiesComponent
+},
+{ path: 'services',
+component: ServicesComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
