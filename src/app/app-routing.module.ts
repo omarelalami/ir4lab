5c65b9fe5e8601d843180c62cfcbd6e7c83988b5
omarelalami <omaralami230@gmail.com>
2022-05-06 15:57:26 +0000

    tRUE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { ServicesComponent } from './services/services.component';
 import { TechnologiesComponent } from './technologies/technologies.component';
 
-const routes: Routes = [{ path: 'landing',
+const routes: Routes = [{ path: 'technologies',
 component: TechnologiesComponent
 },
 { path: 'services',
 component: ServicesComponent
+},
+{ path: '',
+component: AppComponent
+
 }];
 
 @NgModule({
