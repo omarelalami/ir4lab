66c405c3649218511255b2d497c7f744b2585fbe
omarelalami <omaralami230@gmail.com>
2022-05-06 19:57:51 +0000

    contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
+import { useAnimation } from '@angular/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ServicesComponent } from './services/services.component';
 import { TechnologiesComponent } from './technologies/technologies.component';
 
@@ -12,6 +14,10 @@ component: ServicesComponent
 },
 { path: '',
 component: AppComponent
+
+},
+{ path: 'contact',
+component: ContactUsComponent
 
 }];
 
