dd579cf343ea3412344229644d68a8819f0ba232
omarelalami <omaralami230@gmail.com>
2022-05-07 19:20:20 +0000

    footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+//import { famap } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,6 +10,8 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 })
 export class FooterComponent implements OnInit {
   faFacebook = faFacebook;
+  faLinkedinIn=faLinkedinIn;
+  faTwitter=faTwitter;
   constructor() { }
 
   ngOnInit(): void {
