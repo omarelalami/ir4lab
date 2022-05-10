10dd11635f294151655c65fe6036f9ddb0af6f29
Ahmed-Achtatar <ahmed.achtatar@usmba.ac.ma>
2022-05-10 01:56:19 +0100

    about us

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faBullseye } from '@fortawesome/free-solid-svg-icons';
+import { faSuperpowers } from '@fortawesome/free-brands-svg-icons';
+import { faStairs } from '@fortawesome/free-solid-svg-icons';
+import { faInfinity } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUSComponent implements OnInit {
-
+  faBullseye=faBullseye;
+  faSuperpowers=faSuperpowers;
+  faStairs=faStairs
+  faInfinity=faInfinity;
   constructor() { }
 
   ngOnInit(): void {
