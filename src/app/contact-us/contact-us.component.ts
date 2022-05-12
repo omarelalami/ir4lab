de86f55763e1185aeec78ef7d8d9b35492ec72d5
Ahmed-Achtatar <ahmed.achtatar@usmba.ac.ma>
2022-05-12 15:02:55 +0100

    done

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faAt } from '@fortawesome/free-solid-svg-icons';
-
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
@@ -12,6 +14,9 @@ export class ContactUsComponent implements OnInit {
   faLocationDot=faLocationDot;
   faPhone=faPhone;
   faAt=faAt;
+  faTwitter=faTwitter;
+  faFacebook=faFacebookF;
+  faLinkedin=faLinkedinIn;
   constructor() { }
 
   ngOnInit(): void {
